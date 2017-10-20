07b85bcf57511c50838f8f05b4a3fc8856990bfa
tmac1999 <119141701@qq.com>
2017-10-20 14:51:10 +0800

    增加对github的爬取

--- html_parser.py
+++ html_parser.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 
 
 class HtmlParser(object):
-
     def parse(self, page_url, html_cont):
         if page_url is None or html_cont is None:
             return
@@ -19,13 +18,13 @@ class HtmlParser(object):
 
     def _get_new_urls(self, page_url, soup):
         new_urls = set()
-        links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        # links = soup.find_all('a', href=re.compile(r"/view/\d+\.htm"))
+        links = soup.find_all('a', href=re.compile(r"\.html"))
         for link in links:
             new_url = link['href']
             new_full_url = urlparse.urljoin(page_url, new_url)
             new_urls.add(new_full_url)
         return new_urls
-
 
     def _get_new_data(self, page_url, soup):
         res_data = {}
@@ -34,9 +33,19 @@ class HtmlParser(object):
 
         res_data['url'] = page_url
         # <dd class="lemmaWgt-lemmaTitle-title"><h1>Python</h1>
-        title_node = soup.find('dd', class_= "lemmaWgt-lemmaTitle-title").find("h1")
+        title_node = soup.find('dd', class_="lemmaWgt-lemmaTitle-title").find("h1")
         res_data['title'] = title_node.get_text()
 
         summary_node = soup.find('div', class_="lemma-summary")
         res_data['summary'] = summary_node.get_text()
         return res_data
+
+    def parseGithub(self, url, html_cont):
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        result_set = soup.find_all('a',class_="v-align-middle")
+        result_set2 = soup.find_all('a',class_="muted-link")
+        for result_url in result_set:
+            print result_url
+        for result_url in result_set2:
+            print result_url
+        return result_set
--- spider_main.py
+++ spider_main.py
@@ -33,29 +33,24 @@ import html_outputer
 import url_manager
 
 
-
 class SpiderMain(object):
-
     def __init__(self):
         self.urls = url_manager.UrlManager()
         self.downloader = html_downloader.HtmlDownloader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
 
-
-
     def craw(self, root_url):
         count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
-            try :
+            try:
                 new_url = self.urls.get_new_url()
                 print 'craw %d : %s' % (count, new_url)
                 html_cont = self.downloader.download(new_url)
                 new_urls, new_data = self.parser.parse(new_url, html_cont)
                 self.urls.add_new_urls(new_urls)
                 self.outputer.collect_data(new_data)
-
 
                 if count == 1000:
                     break
@@ -65,7 +60,16 @@ class SpiderMain(object):
 
         self.outputer.output_html()
 
+    def crawGithub(self, url):
+        content = self.downloader.download(url)
+        self.parser.parseGithub(url,content)
+
+
 if __name__ == "__main__":
     root_url = "http://baike.baidu.com/view/21087.htm"
     obj_spider = SpiderMain()
     obj_spider.craw(root_url)
+
+url = "https://github.com/search?o=desc&p=3&q=android&s=stars&type=Repositories&utf8=%E2%9C%93";
+obj_spider = SpiderMain()
+obj_spider.crawGithub(url)
