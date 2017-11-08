155619d4bc944b2d1647827676a259933fc22fd6
tmac1999 <119141701@qq.com>
2017-11-08 18:10:53 +0800

    取出含Des列的表

--- TableAnalysis.py
+++ TableAnalysis.py
@@ -4,12 +4,17 @@ if __name__ == '__main__':
     connect = sqlite3.connect('/Users/lianhua/Downloads/MM(1).sqlite')
     cursor = connect.cursor()
     cursor.execute("select name from sqlite_master where type='table' order by name;")
+    # cursor.execute("select * from sqlite_master where name='tablename' and sql like '%fieldname%'; ")
     # cursor.execute("select count(rownum) from [table]")
     # cursor.fetchall()
-    # todo 1建立每个对话表与人名的对应关系 2群聊中人名与id对应 3按聊天消息条数排序，发出与接收百分比 4 群聊中被@ 分析 5饼状图 和 条形图
+
     #
     for row in cursor:
         # print row[0]
-        cursorRowCount = connect.cursor()
-        cursorRowCount.execute("select count(*) from " + row[0])
-        print row[0], ":", cursorRowCount.fetchall()
+        cursorTemp = connect.cursor()
+        # cursorRowCount.execute("select * from sqlite_master where name='"+row[0]+"' and sql like 'Des'; ")
+        # cursorRowCount.execute("select count(*) from " + row[0])
+        cursorTemp.execute("PRAGMA table_info(["+row[0] +"])")
+        if cursorTemp.fetchall()[-1].__contains__('Des') :
+            print row[0], ":", cursorTemp.fetchall()
+
