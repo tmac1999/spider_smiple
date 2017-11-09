a1545398dfe99865c1a156d613afbde1d7225dfa
tmac1999 <119141701@qq.com>
2017-11-09 17:56:48 +0800

    sort table

---
+++ ChatTable.py
@@ -0,0 +1,24 @@
+class ChatTable(object):
+
+    def __str__(self):
+        return self.tableName
+
+    def __init__(self, tableName, myMsgCount, totalMsgCount, percentage):
+        self.tableName = tableName
+        self.myMsgCount = myMsgCount
+        self.otherMsgCount = totalMsgCount
+        self.percentage = percentage
+
+    def __cmp__(self, other):
+        if self.myMsgCount < other.myMsgCount:
+            return -1
+        elif self.myMsgCount > other.myMsgCount:
+            return 1
+        else:
+            return 0
+
+    def __dict__(self):
+        return self.tableName
+
+    def dic(self):
+        return self.tableName+':', self.myMsgCount, '-', self.otherMsgCount, '-', self.percentage
--- TableAnalysis.py
+++ TableAnalysis.py
@@ -2,6 +2,11 @@
 # -*- coding:utf8 -*- 为了支持中文注释
 import sqlite3
 
+from ChatTable import ChatTable
+
+
+def prn_obj(obj):
+    print '\n'.join(['%s:%s' % item for item in obj.__dict__.items()])
 if __name__ == '__main__':
     connect = sqlite3.connect('/Users/lianhua/Downloads/MM.sqlite')
     cursor = connect.cursor()
@@ -16,15 +21,31 @@ if __name__ == '__main__':
         cursorTemp = connect.cursor()
         # cursorRowCount.execute("select * from sqlite_master where name='"+row[0]+"' and sql like 'Des'; ")
         # cursorRowCount.execute("select count(*) from " + row[0])
-        cursorTemp.execute("PRAGMA table_info(["+row[0] +"])")
+        cursorTemp.execute("PRAGMA table_info([" + row[0] + "])")
         fetchall = cursorTemp.fetchall()
-        if fetchall[-1].__contains__('Des') :
+        tableList = []
+        if fetchall[-1].__contains__('Des'):
             cursorMyMsgRowCount = connect.cursor()
             cursorOtherMsgRowCount = connect.cursor()
-            cursorMyMsgRowCount.execute("select count(*) from " + row[0]+" where Des = 0")
-            cursorOtherMsgRowCount.execute("select count(*) from "+row[0])
+            cursorMyMsgRowCount.execute("select count(*) from " + row[0] + " where Des = 0")
+            cursorOtherMsgRowCount.execute("select count(*) from " + row[0])
             msg_row_count_next_ = cursorMyMsgRowCount.next()[0]
             next_ = cursorOtherMsgRowCount.next()[0]
             # cursorRowCount.next() 返回第一行数据，tuple类型，第0号位置即Des=0的行数（自己说话的行数）
-            print row[0], ":", msg_row_count_next_, '-', next_, '-',msg_row_count_next_/float(next_)#分母应该还要减去Type=10000 的msg，因为这类msg都Des=1
+            table = ChatTable(row[0], msg_row_count_next_, next_, msg_row_count_next_ / float(next_))
+            tableList.append(table)
+
+            # print row[0], ":", msg_row_count_next_, '-', next_, '-', msg_row_count_next_ / float(
+            #     next_)  # 分母应该还要减去Type=10000 的msg，因为这类msg都Des=1
+        sorted(tableList)
+        # todo sort not complete
+        for object in tableList:
+            print object.dic()
+
+
+
+
+
+
+
 
