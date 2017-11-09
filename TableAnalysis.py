e1247c9b3470c0a257d1a588a524061eb0f4063d
tmac1999 <119141701@qq.com>
2017-11-09 11:25:52 +0800

    msg Des percentage

--- TableAnalysis.py
+++ TableAnalysis.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*- 为了支持中文注释
 import sqlite3
 
 if __name__ == '__main__':
-    connect = sqlite3.connect('/Users/lianhua/Downloads/MM(1).sqlite')
+    connect = sqlite3.connect('/Users/lianhua/Downloads/MM.sqlite')
     cursor = connect.cursor()
     cursor.execute("select name from sqlite_master where type='table' order by name;")
     # cursor.execute("select * from sqlite_master where name='tablename' and sql like '%fieldname%'; ")
@@ -15,6 +17,14 @@ if __name__ == '__main__':
         # cursorRowCount.execute("select * from sqlite_master where name='"+row[0]+"' and sql like 'Des'; ")
         # cursorRowCount.execute("select count(*) from " + row[0])
         cursorTemp.execute("PRAGMA table_info(["+row[0] +"])")
-        if cursorTemp.fetchall()[-1].__contains__('Des') :
-            print row[0], ":", cursorTemp.fetchall()
+        fetchall = cursorTemp.fetchall()
+        if fetchall[-1].__contains__('Des') :
+            cursorMyMsgRowCount = connect.cursor()
+            cursorOtherMsgRowCount = connect.cursor()
+            cursorMyMsgRowCount.execute("select count(*) from " + row[0]+" where Des = 0")
+            cursorOtherMsgRowCount.execute("select count(*) from "+row[0])
+            msg_row_count_next_ = cursorMyMsgRowCount.next()[0]
+            next_ = cursorOtherMsgRowCount.next()[0]
+            # cursorRowCount.next() 返回第一行数据，tuple类型，第0号位置即Des=0的行数（自己说话的行数）
+            print row[0], ":", msg_row_count_next_, '-', next_, '-',msg_row_count_next_/float(next_)#分母应该还要减去Type=10000 的msg，因为这类msg都Des=1
 
